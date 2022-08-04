f138a7ffebc506cc8d21f5aff200b92dcdffe58c
gunjan1909 <bhanarkaranita@gmail.com>
2022-08-05 00:47:02 +0530

    insertion sort done

--- Insertion Sort/insertion.cpp
+++ Insertion Sort/insertion.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 void insertionSort(vector<int> &arr, int n)
 {
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+      key = arr[i];
+      j = i - 1;
+      while (j >= 0 && arr[j] > key)
+      {
+         arr[j + 1] = arr[j];
+         j--;
+      }
+      arr[j + 1] = key;
+   }
 }
 
 void printArray(vector<int> arr, int size)
